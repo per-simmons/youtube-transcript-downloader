7baa341ba9c2fe5296ec2d494976642fb3bf5c0a
Pat Simmons <pat@persimmons.studio>
2025-03-24 16:01:24 -0400

    Add Vercel deployment configuration

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import uvicorn
+from app.main import app
 
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True) 
+# This is the entry point for Vercel
+app = app 
